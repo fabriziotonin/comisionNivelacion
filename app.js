99d7ef2f2c1adf8bf6d9cac424d18a97fdd1fea1
Fabrizio Tonin <xfitex@gmail.com>
2020-08-14 17:17:22 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Un octal es un numero que esta presedido de un 0 y que va del 1 al 7
+let octal = "01234";
+// Un numero hexadesimal recibe letras desde la A a la F al igual que numeros de 0 al 7 y sirve para los codigo de colores
+let hexadesimal = "FFFFF";
+// Los desimales son los numeros enteros
+let desimal = "10"
+
+// transforma el valor hexadesimal en un numero
+// let yourNumber = parseInt(hexString, 16);
+
+// para convertir un numero parseado de vuelta a su estado hexadesimal
+// let hexString = yourNumber.toString(16);
