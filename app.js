d9de46fa1676d33375a45b0accd3ab727963d710
Fabrizio Tonin <xfitex@gmail.com>
2020-08-22 18:55:43 -0300

    clase de for

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-// let numero = parseInt(prompt("ingrese un numero"));
-// let hexadesimales = "13"
-// console.log(parseInt(hexadesimales))
-//  numeros octales sirbe para los codigos binarios de 3 
-// let ocatal = 0 -7 
 
-// let hexadesimales = A - F y 0 - 7
-// let num1 = parseInt(prompt("ingrese un numero"))
+// template string ` ` no sirve para escribir js y texto en una misma linea ${codigo js}
+// let nombre = "José"
+// let nombre2 = 'Juanita'
+// console.log("hola como estas " + nombre + " y hola " + nombre2)
+// console.log(`hola como estas ${nombre} y hola como estas ${nombre2}`)
 
-// if(isNaN(num1)){
-//     console.log("no es un numero") 
-// }else{
-//     console.log(num1)
-// }
+let num1 = parseInt(prompt("ingresa un numero"))
+// 1- let i = 0 que nos permite
+// 2- es la condicion que nos permite ejecutar un numero de veces el codigo
 
-let nombre = "Fabrizio"
+// NAN = Not a number no es un numero
+// isNaN()
+for( let i = 0; i <= 10 ; i++ ){
+  if(!isNaN(num1)){
+    // let resultado = num1 * i
+    document.write(`${num1} y ${i} es = ${num1 * i}`)
+    document.write("<br>");
+  }else{
+    alert("No es un numero");
+    break;
+  }
+}
 
-let nombreMayusculas = nombre.toUpperCase()
-let nombreMinusculas = nombre.toLowerCase()
-
-let letrasUna = nombre.charAt(3)
-let cantidadDeLetras = nombre.length
-
-let ultimaLetra = nombre.charAt(nombre.length - 1)
-
-let subStr = nombre.substr(0,3)
---
+++ ejercicios/datos.js
@@ -0,0 +1,26 @@
+// let numero = parseInt(prompt("ingrese un numero"));
+// let hexadesimales = "13"
+// console.log(parseInt(hexadesimales))
+//  numeros octales sirbe para los codigos binarios de 3 
+// let ocatal = 0 -7 
+
+// let hexadesimales = A - F y 0 - 7
+// let num1 = parseInt(prompt("ingrese un numero"))
+
+// if(isNaN(num1)){
+//     console.log("no es un numero") 
+// }else{
+//     console.log(num1)
+// }
+
+let nombre = "Fabrizio"
+
+let nombreMayusculas = nombre.toUpperCase()
+let nombreMinusculas = nombre.toLowerCase()
+
+let letrasUna = nombre.charAt(3)
+let cantidadDeLetras = nombre.length
+
+let ultimaLetra = nombre.charAt(nombre.length - 1)
+
+let subStr = nombre.substr(0,3)
