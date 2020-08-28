f24e0772747eff225ec17b4c8d4065770371559b
Fabrizio Tonin <xfitex@gmail.com>
2020-08-28 19:46:16 -0300

    clase de funciones 28/08

---
+++ ejercicios/funciones.js
@@ -0,0 +1,27 @@
+// funciones expresivas 
+// function sumar(numero){
+
+// if(isNaN(numero)){
+//   console.error("numero no valido")
+// }else{
+//   for(let i = 0 ; i <= 10; i++){
+//     console.log(`la tabla del ${numero} multiplicado por ${i} es = ${i * numero}`)
+//   }
+// }
+// }
+
+// let numeroIngresado = parseInt((prompt("ingrese un valor")));
+
+// sumar(numeroIngresado);
+
+// Funciones Anonimas Callbacks
+
+let saludar = function(nombre){
+  console.log("Hola " + nombre + " " + numero)
+  }
+  
+  console.log(numero)
+  
+  saludar("Jimena")
+  
+  
